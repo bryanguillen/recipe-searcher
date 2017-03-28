6c7261d712125ba321230402b143ec5a21205e43
Bryan <artificialknowledge217@gmail.com>
2017-03-28 10:59:47 -0400

    results template html

--- js/app.js
+++ js/app.js
@@ -15,8 +15,20 @@ function getDataFromAPI(searchTerm, callback) {
 }
 
 function displayDataFromAPI(data) {
-	console.log(data);
-	//so far returns jsonp successfully
+	//get the title, picture, ingredients, and then calories information 
+	//along with serving. 
+	var recipeName = data.hits[0].recipe.label;
+	var img = data.hits[0].recipe.image;
+	var ingredients = data.hits[0].recipe.ingredientLines;
+	var servings = data.hits[0].recipe.yield;
+	var caloricIntake = data.hits[0].recipe.calories;
+	var diet = data.hits[0].recipe.dietLabels;
+	console.log(recipeName);
+	console.log(img);
+	console.log(ingredients);
+	console.log(servings);
+	console.log(caloricIntake);
+	console.log(diet);
 }
 
 //handler 
