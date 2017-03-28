06a205d5d4b2e210ba767c526240d4a79de60b2f
Bryan <artificialknowledge217@gmail.com>
2017-03-27 22:47:32 -0400

    complete getDataFromAPI & displayDataFromAPI functions

--- js/app.js
+++ js/app.js
@@ -1,17 +1,28 @@
 var URL_END_POINT = 'https://api.edamam.com/search';
 
-function getDataFromAPI(URL_END_POINT, callback) {
-	//do something here
+function getDataFromAPI(searchTerm, callback) {
+	var settings = {
+		url: URL_END_POINT,
+		data: {
+			app_id: 'b911c081',
+			app_key: 'ada25e7ca4cf4253efd61e25c30c8fa3',
+			q: searchTerm,
+		},
+		success: callback
+	}
+	$.ajax(settings);
+}
+
+function displayDataFromAPI(data) {
+	console.log(data);
+	//so far returns jsonp successfully
 }
 
 function submitHandler() {
 	$('main').on('click', '.js-search-recipe', function(event) {
 		event.preventDefault();
-		//here, collect input and get data from query
-		//TODO: Figure out home for api code.
-		//it is above submit handler!!!
-		//traverse food flexible enough for if another input field is added
-		var food = $(this).parents('form').find('.food').val();
+		var query = $('main').find('.food-query').val();
+		getDataFromAPI(query, displayDataFromAPI);
 	});
 }
 
